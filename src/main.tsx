351d6c6eb9415f42856a98f02eedd60222592e83
origami-z <davidcui2@me.com>
2022-12-23 19:05:26 +0000

    Bump all dep versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "antd/dist/antd.css";
+import { createRoot } from "react-dom/client";
+import "antd/dist/reset.css";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
